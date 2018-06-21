19bd74ff336ed5bcb54079eb30b69bc212a2bc2a
george <123@321.com>
2018-06-21 22:37:53 +0300

    modified exercise 0.3

--- ch0/0.3/main.cpp
+++ ch0/0.3/main.cpp
@@ -1,8 +1,18 @@
+/* 0-3. The string literal "\t" represents a tab character;
+ * different C++ implementations display tabs in different ways.
+ * Experiment with your implementation to learn how it treats tabs.
+ */
+
 #include <iostream>
 
 int main() {
   std::cout << "\tTesting \\t escape sequence in the beginning.\n";
   std::cout << "Testing \\t escape\tsequence in the middle of the sentence\n";
   std::cout << "Testing \\t escape sequence in the end of the sentence\t\n";
+  std::cout << "Testing\t\t2 \\t escape sequences in the middle of"
+               " the sentence\n";
+
+  std::cout << "\t\tTesting 2 \\t escape sequences in the beginning of"
+               " the sentence\n";
   return 0;
 }
